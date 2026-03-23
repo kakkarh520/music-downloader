049242cf1f9e34733dada62a29347d52e6caf22d
kakkarh520 <kakkarh520@gmail.com>
2026-03-23 09:24:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
-from flask import Flask, render_template, request, jsonify
-import yt_dlp
-import os
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
+
+CLIENT_ID = "6ea226f7"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/songs")
+def songs():
+    url = f"https://api.jamendo.com/v3.0/tracks/?client_id={CLIENT_ID}&format=json&limit=12"
+    response = requests.get(url)
+    data = response.json()
 
-@app.route("/search")
-def search():
-    query = request.args.get("q")
-
-    ydl_opts = {
-        "quiet": True,
-        "extract_flat": True,
-        "skip_download": True
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(f"ytsearch6:{query}", download=False)
-
-    songs = []
-
-    for v in result["entries"]:
-        songs.append({
-            "title": v["title"],
-            "id": v["id"],
-            "thumbnail": f"https://img.youtube.com/vi/{v['id']}/hqdefault.jpg",
-            "url": f"https://www.youtube.com/watch?v={v['id']}"
+    tracks = []
+    for track in data["results"]:
+        tracks.append({
+            "title": track["name"],
+            "artist": track["artist_name"],
+            "audio": track["audio"],
+            "image": track["album_image"]
         })
 
-    return jsonify(songs)
-
+    return jsonify(tracks)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
